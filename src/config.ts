2337e54feec6006bab11cba208839908f21c618c
aabbcc <iyranly@163.com>
2024-10-29 12:46:16 +0800

    Optimize the code to make it more convenient to use.

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const SHADOWLOCK_CODEHASH = "0x6361d4b20d845953d9c9431bbba08905573005a71e2a2432e7e0e7c685666f24";
+
+export const COMPOSE_CELL_DEPS = {
+    outPoint: {
+        txHash: "0x2d8a311a55e42d7d6810610149f6e125f0d292112f8ed4a21177aec05da2b905",
+        index: 0x0,
+    },
+    depType: "code",
+}
